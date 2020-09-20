1515b46810fcaa5a4f81ea32f7326aad458a26b6
yp6497 <59597619+yp6497@users.noreply.github.com>
2020-09-20 15:26:58 +0300

    f

--- app/src/main/java/com/example/targil_081/hazaga.java
+++ app/src/main/java/com/example/targil_081/hazaga.java
@@ -4,58 +4,74 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class hazaga extends AppCompatActivity {
 
-    //ListView lv;
-    TextView wa,whc,m,c;
-    String s;
-    float a,h;
-    //int sum=0,cfola=1;
+    public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    String [] eivar= new String[20];
+        ListView lv;
+        TextView wa, whc, m, c;
+        String s;
+        float a, h;
+        int sum=0,cfola=1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hazaga);
+        String[] eivar = new String[20];
 
-        //lv=(ListView) findViewById(R.id.lv);
-        wa=findViewById(R.id.wa);
-        whc=findViewById(R.id.wh);
-        m=findViewById(R.id.m);
-        c=findViewById(R.id.c);
 
-        //ArrayAdapter<String> adp = new ArrayAdapter<String>(this,
-          //     R.layout.support_simple_spinner_dropdown_item, eivar);
-        //lv.setAdapter(adp);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_hazaga);
 
-        start();
-    }
+            lv = (ListView) findViewById(R.id.lv);
+            wa = findViewById(R.id.wa);
+            whc = findViewById(R.id.wh);
+            m = findViewById(R.id.m);
+            c = findViewById(R.id.c);
 
-    private void start() {
-        Intent gi = getIntent();
-        s=gi.getStringExtra("s");
-        a=gi.getFloatExtra("a",1);
-        h=gi.getFloatExtra("h",1);
-        wa.setText("the first: "+a);
-        whc.setText("the hfresh: "+h);
-        /*
-        float sum= h;
-        if(s=="h") {
-            for(int i=0;i<=20;i++) {
-                //eivar[i]= String.valueOf(sum+a);
+            lv.setOnItemClickListener(this);
+            lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+            ArrayAdapter<String> adp = new ArrayAdapter<String>(this,
+                    R.layout.support_simple_spinner_dropdown_item, eivar);
+            lv.setAdapter(adp);
+
+            start();
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            m.setText("position:" + position);
+        }
+
+        private void start() {
+            Intent gi = getIntent();
+            s = gi.getStringExtra("s");
+            a = gi.getFloatExtra("a", 1);
+            h = gi.getFloatExtra("h", 1);
+            wa.setText("the first: " + a);
+            whc.setText("the hfresh: " + h);
+
+            //float sum= h;
+            if (s == "h") {
+                for (int i = 0; i <= 20; i++) {
+                     eivar[i]= String.valueOf(sum+a);
+                    sum++;
+                }
+
+            } else if (s == "הנדסית") {
                 sum++;
+
+
             }
-
         }
-        else if(s=="הנדסית") {
-            sum++;
-    */
-
-        }
-
     }
+}
+
+
